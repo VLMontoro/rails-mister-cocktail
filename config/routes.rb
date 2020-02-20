54e4c5394e1de8c1eb55ef09f9df39d621eee2c7
Veronica Lopez <v.lopezmontoro@gmail.com>
2020-02-20 16:58:40 +0100

    Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+
+  root to: "cocktails#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+
+end
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
