137eb3fc1cc3370d6b257fb831b5bf3d08cb673a
Veronica Lopez <v.lopezmontoro@gmail.com>
2020-02-21 15:32:26 +0100

    Cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -35,6 +35,6 @@ before_action :set_cocktail, only: [:show, :destroy]
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
