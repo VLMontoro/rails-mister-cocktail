a33259481365e169c64b5d8ec39a3166b7fe6371
Veronica Lopez <v.lopezmontoro@gmail.com>
2021-05-01 21:09:41 +0200

    test

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,13 @@
 class CocktailsController < ApplicationController
-before_action :set_cocktail, only: [:show, :destroy]
-
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
+    @review = Review.new
   end
-
-  # def show
-  #   @cocktail = Cocktail.find(params[:id])
-  #   @dose = Dose.new
-  #   @review = Review.new
-  # end
 
   def new
     @cocktail = Cocktail.new
@@ -24,17 +18,13 @@ before_action :set_cocktail, only: [:show, :destroy]
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'new'
     end
   end
 
   private
 
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
-
   def cocktail_params
-    params.require(:cocktail).permit(:name, photos: [])
+    params.require(:cocktail).permit(:name)
   end
 end
